1c194a2127c9f3af89c8abf20785ffcd33b7860a
delayedaa <delayedaa@gmail.com>
2021-02-24 17:39:02 -0600

    Add user delete confirmation

--- client/src/components/DeleteUserForm.js
+++ client/src/components/DeleteUserForm.js
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const DeleteUserForm = (props) => {
-  const handleClick = async () => {
+  const [confirm, setConfirm] = useState(false);
+
+  useEffect(() => {
+    setConfirm(false);
+  }, [props.currentUser]);
+
+  const renderDeleteForm = () => {
+    if (!confirm) {
+      return <button onClick={toggleConfirm}>Delete</button>;
+    } else {
+      return (
+        <div>
+          <p>Are you sure?</p>
+          <button onClick={deleteUser}>Yes</button>
+          <button onClick={toggleConfirm}>No</button>
+        </div>
+      );
+    }
+  };
+
+  const toggleConfirm = () => {
+    setConfirm(!confirm ? true : false);
+  };
+
+  const deleteUser = async () => {
     try {
       const res = await axios.delete(`http://localhost:8000/users/${props.currentUser._id}`);
       alert('User deleted.');
@@ -14,7 +39,7 @@ export const DeleteUserForm = (props) => {
   return (
     <div>
       <h3>Delete User</h3>
-      <button onClick={handleClick}>Delete</button>
+      {renderDeleteForm()}
     </div>
   );
 };
